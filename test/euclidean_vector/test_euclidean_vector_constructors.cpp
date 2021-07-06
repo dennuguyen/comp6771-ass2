d623c27bc18956379dce8d9885cfda532b3d6325
z5206032 <z5206032@cse.unsw.edu.au>
2021-07-07 01:50:33 +1000

    Clean up move test

--- test/euclidean_vector/test_euclidean_vector_constructors.cpp
+++ test/euclidean_vector/test_euclidean_vector_constructors.cpp
@@ -247,16 +247,12 @@ TEST_CASE("Move constructor should instantiate correctly") {
 	SECTION("Move empty euclidean vector") {
 		auto euc_vec1 = comp6771::euclidean_vector({});
 		auto euc_vec2 = comp6771::euclidean_vector(std::move(euc_vec1));
-		CHECK(euc_vec1.dimensions() == 0);
 		CHECK(euc_vec2.dimensions() == 0);
 	}
 
 	SECTION("Move 1 dimension euclidean vector") {
 		auto euc_vec1 = comp6771::euclidean_vector({3.4});
 		auto euc_vec2 = comp6771::euclidean_vector(std::move(euc_vec1));
-
-		CHECK(euc_vec1.dimensions() == 0);
-
 		CHECK(euc_vec2.at(0) == Approx(3.4));
 		CHECK(euc_vec2.dimensions() == 1);
 	}
@@ -264,9 +260,6 @@ TEST_CASE("Move constructor should instantiate correctly") {
 	SECTION("Move multi-dimension euclidean vector") {
 		auto euc_vec1 = comp6771::euclidean_vector({3.4, 6.1, 7.184181, 5.51});
 		auto euc_vec2 = comp6771::euclidean_vector(std::move(euc_vec1));
-
-		CHECK(euc_vec1.dimensions() == 0);
-
 		CHECK(euc_vec2.at(0) == Approx(3.4));
 		CHECK(euc_vec2.at(1) == Approx(6.1));
 		CHECK(euc_vec2.at(2) == Approx(7.184181));
