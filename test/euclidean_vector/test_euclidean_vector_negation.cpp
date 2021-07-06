9062e0ce438b73fa191ecaa1a15dd8c27491b867
z5206032 <z5206032@cse.unsw.edu.au>
2021-07-07 02:41:57 +1000

    Add test cases

---
+++ test/euclidean_vector/test_euclidean_vector_negation.cpp
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// TESTING RATIONALE
+//
+// This set of test cases are related to the euclidean_vector negation operator.
+//
+// Refer to test_euclidean_vector_constructors.cpp for TEST DESIGN RATIONALE.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "comp6771/euclidean_vector.hpp"
+
+#include <catch2/catch.hpp>
+#include <sstream>
+
+TEST_CASE("Negation should return negative copy") {
+	SECTION("Empty euclidean vector") {
+		auto euc_vec1 = comp6771::euclidean_vector({});
+		auto euc_vec2 = -euc_vec1;
+		CHECK(euc_vec2.dimensions() == 0);
+	}
+
+	SECTION("Zero euclidean vector") {
+		auto euc_vec1 = comp6771::euclidean_vector(3, 0.0);
+		auto euc_vec2 = -euc_vec1;
+		CHECK(euc_vec2.at(0) == Approx(0.0));
+		CHECK(euc_vec2.at(1) == Approx(0.0));
+		CHECK(euc_vec2.at(2) == Approx(0.0));
+		CHECK(euc_vec2.dimensions() == 3);
+	}
+
+	SECTION("1 dimension euclidean vector") {
+		auto euc_vec1 = comp6771::euclidean_vector({1.0});
+		auto euc_vec2 = -euc_vec1;
+		CHECK(euc_vec2.at(0) == Approx(-1.0));
+	}
+
+	SECTION("2 dimension euclidean vector") {
+		auto euc_vec1 = comp6771::euclidean_vector({-42.0, 21.3});
+		auto euc_vec2 = -euc_vec1;
+		CHECK(euc_vec2.at(0) == Approx(42.0));
+		CHECK(euc_vec2.at(1) == Approx(-21.3));
+	}
+
+	SECTION("3 dimension euclidean vector") {
+		auto euc_vec1 = comp6771::euclidean_vector({-2.032, 8.3293, 12.429});
+		auto euc_vec2 = -euc_vec1;
+		CHECK(euc_vec2.at(0) == Approx(2.032));
+		CHECK(euc_vec2.at(1) == Approx(-8.3293));
+		CHECK(euc_vec2.at(2) == Approx(-12.429));
+	}
+}
