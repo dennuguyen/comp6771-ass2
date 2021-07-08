3e4835b21eb7dbc39cbe8803f67ae0784bceb3b0
z5206032 <z5206032@cse.unsw.edu.au>
2021-07-08 18:45:05 +1000

    Add referal to test rationale

--- test/euclidean_vector/test_euclidean_vector_assignments.cpp
+++ test/euclidean_vector/test_euclidean_vector_assignments.cpp
@@ -5,28 +5,7 @@
 // This set of test cases are related to the copy and move assignment of a euclidean_vector to
 // another euclidean_vector.
 //
-// TEST DESIGN RATIONALE
-//
-// The approach to testing design is property-based. The overall test design is done through
-// abstraction of properties and conditions. A property is a particular characteristic of the output
-// that is true. A condition is a scenario/case where the property can be exhibited.
-//
-// Hierarchy of the test design:
-// 	- test file: collection of similar properties or a single property that we want to test
-// 	- test case: a particular condition about the property that is true i.e. something should do
-// something
-// 	- test section: various cases which will give an output to show the property to be true for a
-// condition we are testing.
-//
-// I use this particular hierarchy because it neatly separates properties, conditions, and
-// use-cases.
-//
-// The TEST_CASE names are also descriptive to give an idea of what the test case is for and follows
-// a "something should something" format. Therefore TEST_CASE is not commented as it will be
-// redundant to rewrite what the test case is for. Comments will only be given at the top of the
-// file to clarify why we are testing a particular property.
-//
-// Some test files will only have a single test case if the property to be tested is simple.
+// Refer to test_euclidean_vector_constructors.cpp for TEST DESIGN RATIONALE.
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
